66e58ac616e3407af38da9d5625bd611925cd4a9
ronar01 <105427300+ronar01@users.noreply.github.com>
2024-01-17 16:40:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert("")
+alert("Oba oba");
+let numSecreto = parseInt(Math.random() * 10 + 1);
+let palpite = 0;
+let tentativas = 0;
+while(palpite != numSecreto){
+    palpite = prompt("Diz um número meu consagrado");
+    tentativas++;
+    if(palpite == numSecreto){
+        break
+    }else{
+        if(palpite > numSecreto){
+            alert("É menor que " + palpite);
+        }
+        else{
+            alert("É maior que "+ palpite)
+        }
+    }
+}
+palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+alert("Você adivinhou o número " + numSecreto + ", após " + tentativas + " " + palavraTentativa)
+
+
